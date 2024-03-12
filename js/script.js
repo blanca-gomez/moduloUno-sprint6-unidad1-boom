898f3c7b12c950026afbed1639a0c1c8189e4610
Blanca Gómez <blanca_gp_96@hotmail.com>
2024-03-12 11:38:04 +0100

    Ejercicio terminado

--- js/script.js
+++ js/script.js
@@ -1,64 +1,52 @@
-//Obtengo elementos del DOM
-
-const user = document.getElementById('userInput');
-const countDown = document.getElementById('countdown');
 const resultContainer = document.getElementById('result');
+const restartButton = document.getElementById('restart');
 
-const buttonReset = document.getElementById('restart'); 
-const boombresults=document.createElement('p');
+let countdown;
+let randomNumber;
 
-
-let counter = 5;
-let interval;
-
-
-// Puedes usar `setInterval()` para generar el contador de 5 segundos (recuerda que es del 5 al 0, por tanto el intervalo debería ser uno más) 5, 4, 3, 2, 1, 0 ...
-
-
-function startCountDown () {
-    return new Promise((resolve,reject) => {
-        interval = setInterval(() => {
-            countDown.innerHTML = `Cuenta atras: ${counter} segundos`;
-            counter = counter -1;
-            if(counter<0){
-                clearInterval(interval);
-                counter = 5;
-                resolve();
+function startGame(){
+    return new Promise ((resolve) => {
+        randomNumber = Math.floor(Math.random()*3)+1;
+        console.log('El numero aleatorio es: ' + randomNumber);
+        //cuenta atrás 5 segundos
+        countdown = 5;
+        let intervalCountDown = setInterval(() => {
+          countdown --;
+          countdownCounter();
+          if (countdown === 0){
+            clearInterval(intervalCountDown);
+            resolve();
             }
-        },1000)   
+        }, 1000)  
     })
-}
-
-function boombGame(){
-    const userValue = user.value;
-    countDown.innerHTML = '';
-    resultContainer.innerText = '';
-    startCountDown()
-        .then(() => {
-           const randomnumber = Math.floor(Math.random()*3)+1;
-            if(userValue === 1 || userValue === 2 || userValue === 3 ){
-                boombresults.innerText = `tu número ${user.value} es el mismo que ${randomnumber}`;
-            }
-            if (userValue === randomnumber){
-                boombresults.innerText = 'Has salvado el mundo!'
-            }else{
-                boombresults.innerText = 'La bomba ha estallado!'
-            } 
-            resultContainer.appendChild(boombresults);
-        })
-        .catch((error) =>{
-        console.log('Ha ocurrido un error', error)
-    });
-    
 };
 
-buttonReset.addEventListener('click', () => {
-    // Reinicia el mensaje de resultado
-    clearInterval(interval)
-    // Inicia un nuevo juego
-    boombGame();
-});
+function countdownCounter () {
+  document.getElementById('countdown').innerText = 'cuenta atrás: ' + countdown;
+};
 
-boombGame();
+function results (){
+  const userInput = parseInt(document.getElementById('userInput').value);
+  if (userInput === randomNumber){
+    resultContainer.innerText = '¡Has salvado el mundo! \n Tu número era ' + userInput + ' y el número aleatorio era '+ randomNumber;
+    const crownIcon = document.createElement('img');
+    crownIcon.src='/moduloUno-sprint6-unidad1-boom/img/corona.png';
+    crownIcon.alt ='corona';
+    resultContainer.appendChild(crownIcon);
+  }else{
+    resultContainer.innerText = 'La bomba ha estallado \n Tu número era ' + userInput + ' y el número aleatorio era '+ randomNumber;
+  }
+};
 
-  
+function restartGame(){
+  document.getElementById('countdown').innerText = '';
+  document.getElementById('result').innerText = '';
+  document.getElementById('userInput').value = '';
+
+  startGame()
+    .then(() => {
+        results();
+    })
+};
+
+restartButton.addEventListener('click', restartGame);
